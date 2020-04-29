9da161214a2699b3c1cef245daf46725cea2e5bc
Rohit Narurkar <rohit.narurkar@protonmail.com>
2020-04-29 13:47:51 +0530

    compression and decompression for both curves

--- src/curves.ts
+++ src/curves.ts
@@ -3,26 +3,125 @@ const BN = require('bn.js')
 const EC = require('elliptic')
 const { randomHex } = require('web3-utils')
 
-export abstract class EllipticCurve {
-  curve: typeof EC.curve.short.ShortCurve
+type ShortCurve = typeof EC.curve.short.ShortCurve
+type Point = typeof EC.curve.short.Point
 
-  randomScalar (): typeof BN {
+export abstract class EllipticCurve {
+  curve: ShortCurve
+
+  randomScalar(): typeof BN {
     const groupModulus = new BN(this.curve.n, 16)
     const groupReduction = BN.red(groupModulus)
     return new BN(randomHex(32), 16).toRed(groupReduction)
   }
+
+  pointFromScalar(s: typeof BN): Point {
+    return this.curve.g.mul(s)
+  }
+
+  randomPoint(): Point {
+    const r = this.randomScalar()
+    return this.pointFromScalar(r)
+  }
+
+  abstract decompressHex(h: typeof BN): Point
 }
 
 export class Bn254 extends EllipticCurve {
-  constructor () {
+  compressionMask: typeof BN
+
+  constructor() {
     super()
     this.curve = bn128.curve
+    this.compressionMask = new BN(
+      '8000000000000000000000000000000000000000000000000000000000000000',
+      16
+    )
+  }
+
+  compressToHex(p: Point): string {
+    let compressed = p.getX()
+    if (p.getY().testn(0)) {
+      compressed = compressed.or(this.compressionMask)
+    }
+    return compressed.toString(16, 64)
+  }
+
+  decompressHex(h: string): Point {
+    const compressed = new BN(h, 16)
+    const yBit = compressed.testn(255)
+    const x = compressed.maskn(255).toRed(this.curve.red)
+    const y2 = x
+      .redSqr()
+      .redMul(x)
+      .redIAdd(this.curve.b)
+    const yRoot = y2.redSqrt()
+    if (
+      yRoot
+        .redSqr()
+        .redSub(y2)
+        .fromRed()
+        .cmpn(0) !== 0
+    ) {
+      throw new Error('x^3 + 3 not a square, malformed input')
+    }
+    let y = yRoot.fromRed()
+    if (Boolean(y.isOdd()) !== Boolean(yBit)) {
+      y = this.curve.p.sub(y)
+    }
+    return this.curve.point(x.fromRed(), y)
   }
 }
 
 export class Secp256k1 extends EllipticCurve {
-  constructor () {
+  evenCompressionMask: typeof BN
+  oddCompressionMask: typeof BN
+
+  constructor() {
     super()
     this.curve = new EC.ec('secp256k1').curve
+    this.evenCompressionMask = new BN(
+      '020000000000000000000000000000000000000000000000000000000000000000',
+      16
+    )
+    this.oddCompressionMask = new BN(
+      '030000000000000000000000000000000000000000000000000000000000000000',
+      16
+    )
+  }
+
+  compressToHex(p: Point): string {
+    let compressed = p.getX()
+    if (p.getY().testn(0)) {
+      compressed = compressed.or(this.oddCompressionMask)
+    } else {
+      compressed = compressed.or(this.evenCompressionMask)
+    }
+    return compressed.toString(16, 66)
+  }
+
+  decompressHex(h: string): Point {
+    const compressed = new BN(h, 16)
+    const yBit = compressed.testn(256)
+    const x = compressed.maskn(256).toRed(this.curve.red)
+    const y2 = x
+      .redSqr()
+      .redMul(x)
+      .redIAdd(this.curve.b)
+    const yRoot = y2.redSqrt()
+    if (
+      yRoot
+        .redSqr()
+        .redSub(y2)
+        .fromRed()
+        .cmpn(0) !== 0
+    ) {
+      throw new Error('x^3 + 7 not a square, malformed input')
+    }
+    let y = yRoot.fromRed()
+    if (Boolean(y.isOdd()) !== Boolean(yBit)) {
+      y = this.curve.p.sub(y)
+    }
+    return this.curve.point(x.fromRed(), y)
   }
 }
--- test/curves.test.ts
+++ test/curves.test.ts
@@ -1,3 +1,6 @@
+const assert = require('assert').strict
+const BN = require('bn.js')
+
 import { Bn254, Secp256k1 } from '../src/curves'
 
 describe('EllipticCurve.randomScalar', () => {
@@ -9,5 +12,106 @@ describe('EllipticCurve.randomScalar', () => {
   it('Creates a random scalar using Secp256k1', () => {
     let secp256k1 = new Secp256k1()
     let r = secp256k1.randomScalar()
+  })
+})
+
+describe('EllipticCurve.pointFromScalar', () => {
+  it('Generates a point given a scalar for Bn254', () => {
+    let bn254 = new Bn254()
+    let r = bn254.randomScalar()
+    let p = bn254.pointFromScalar(r)
+  })
+
+  it('Generates a point given a scalar for Secp256k1', () => {
+    let secp256k1 = new Secp256k1()
+    let r = secp256k1.randomScalar()
+    let p = secp256k1.pointFromScalar(r)
+  })
+})
+
+describe('EllipticCurve.randomPoint', () => {
+  it('Generates a random point for Bn254', () => {
+    let bn254 = new Bn254()
+    let p = bn254.randomPoint()
+  })
+
+  it('Generates a random point for Secp256k1', () => {
+    let secp256k1 = new Secp256k1()
+    let p = secp256k1.randomPoint()
+  })
+})
+
+describe('EllipticCurve.compressToHex', () => {
+  it('Compresses a point to hex string for Bn254', () => {
+    let bn254 = new Bn254()
+    let x = new BN('1', 10)
+    let y = new BN('2', 10)
+    let p = bn254.curve.point(x, y)
+    let c = bn254.compressToHex(p)
+    assert.deepEqual(c, '0000000000000000000000000000000000000000000000000000000000000001')
+
+    let x2 = new BN('1', 10)
+    let y2 = new BN('2', 10)
+    y2 = bn254.curve.p.sub(y2)
+    let p2 = bn254.curve.point(x2, y2)
+    let c2 = bn254.compressToHex(p2)
+    assert.deepEqual(c2, '8000000000000000000000000000000000000000000000000000000000000001')
+  })
+
+  it('Compresses a point to hex string for Secp256k1', () => {
+    let secp256k1 = new Secp256k1()
+    let x = new BN('79be667ef9dcbbac55a06295ce870b07029bfcdb2dce28d959f2815b16f81798', 16)
+    let y = new BN('483ada7726a3c4655da4fbfc0e1108a8fd17b448a68554199c47d08ffb10d4b8', 16)
+    let p = secp256k1.curve.point(x, y)
+    let c = secp256k1.compressToHex(p)
+    assert.deepEqual(c, '0279be667ef9dcbbac55a06295ce870b07029bfcdb2dce28d959f2815b16f81798')
+
+    let x2 = new BN('109b604bbe55ec2eefdb00828ba806dabedc0096d7f6857078e9365535b52812', 16)
+    let y2 = new BN('7fbb07e07e592186eec8d7b1ecb51c228ec4e58166ceee55ff5aff0aac527f67', 16)
+    let p2 = secp256k1.curve.point(x2, y2)
+    let c2 = secp256k1.compressToHex(p2)
+    assert.deepEqual(c2, '03109b604bbe55ec2eefdb00828ba806dabedc0096d7f6857078e9365535b52812')
+  })
+})
+
+describe('EllipticCurve.decompressHex', () => {
+  it('Decompresses a hex encoding to point for Bn254', () => {
+    let bn254 = new Bn254()
+    let hex1 = '1'
+    let p1 = bn254.decompressHex(hex1)
+    assert(p1.eq(bn254.curve.g))
+
+    let hex2 = '8000000000000000000000000000000000000000000000000000000000000001'
+    let p2 = bn254.decompressHex(hex2)
+    assert(p2.eq(bn254.curve.g.neg()))
+
+    // x^3 + 3 = 3 (not a square)
+    let hex3 = '0000000000000000000000000000000000000000000000000000000000000000'
+    assert.throws(function() {
+      bn254.decompressHex(hex3)
+    }, Error('x^3 + 3 not a square, malformed input'))
+  })
+
+  it('Decompresses a hex encoding to point for Secp256k1', () => {
+    let secp256k1 = new Secp256k1()
+    let hex1 = '0279be667ef9dcbbac55a06295ce870b07029bfcdb2dce28d959f2815b16f81798'
+    let hex2 = '279be667ef9dcbbac55a06295ce870b07029bfcdb2dce28d959f2815b16f81798'
+    let p1 = secp256k1.decompressHex(hex1)
+    let p2 = secp256k1.decompressHex(hex2)
+    assert(p1.eq(secp256k1.curve.g))
+    assert(p2.eq(secp256k1.curve.g))
+
+    let hex3 = '03109b604bbe55ec2eefdb00828ba806dabedc0096d7f6857078e9365535b52812'
+    let p3 = secp256k1.decompressHex(hex3)
+    let expectedX = new BN('109b604bbe55ec2eefdb00828ba806dabedc0096d7f6857078e9365535b52812', 16)
+    let expectedY = new BN('7fbb07e07e592186eec8d7b1ecb51c228ec4e58166ceee55ff5aff0aac527f67', 16)
+    assert(p3.getX().eq(expectedX))
+    assert(p3.getY().eq(expectedY))
+
+    // x^3 + 7 = 7 (not a square)
+    let hex4 = '020000000000000000000000000000000000000000000000000000000000000000'
+    assert.throws(function() {
+      secp256k1.decompressHex(hex4)
+    }, Error('x^3 + 7 not a square, malformed input'))
   })
 })
